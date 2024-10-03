7440905dec35c066f20032f43ac6bb1448c7ea7b
Your Name <you@example.com>
2024-10-03 08:37:42 +0530

    update data

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1:27017/interviewDB')
 
-mongoose.connect(`${process.env.DB_URL}/interviewDB`)
+mongoose.connect(`${process.env.DB_URL}interviewDB`)
   .then(() => console.log('Connected!'))
   .catch((err) => console.log(err.message))
 
